0c779917e995e13aaf6e25c40acb5f7f48d795be
Iana <malovayai@gmail.com>
2020-11-09 01:11:51 +0300

    1:11 ночи)0

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,6 +15,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using TasksList.Models;
 using TasksList.Services;
+using System.Windows.Threading;
+using System.Media;
 
 namespace TasksList
 {
@@ -23,6 +25,13 @@ namespace TasksList
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int MaxTimerCounterWork;
+        private int MaxTimerCounterRelax;
+        const int MinTimerCounter = -1;
+        DispatcherTimer _dispTimer1;
+        DispatcherTimer _dispTimer2;
+        int _dispTimerCounterWork;
+        int _dispTimerCounterRelax;
         private readonly string PATH = $"{Environment.CurrentDirectory}\\todoDataList.json";
         private BindingList<ToDoModel> _toDo;
         private FileIOService _fileIOService;
@@ -98,6 +107,66 @@ namespace TasksList
             {
                 e.Handled = true; // если пробел, отклоняем ввод
             }
+        }
+
+        void dispTimer_Tick1(object sender, EventArgs e)
+        {
+            //сначала выводится значение, потом инкрементируется (не для всех очевидно)
+            Timer1.Text = (_dispTimerCounterWork--).ToString();
+            if (_dispTimerCounterWork == MinTimerCounter)
+            {
+                _dispTimer1.Stop();
+                SystemSounds.Beep.Play();
+            }
+
+        }
+        private void StartWork(object sender, RoutedEventArgs e)
+        {
+            MaxTimerCounterWork = Convert.ToInt32(TextBox1.Text);
+            _dispTimerCounterWork = MaxTimerCounterWork;
+            _dispTimer1 = new DispatcherTimer();
+            _dispTimer1.Interval = TimeSpan.FromSeconds(1);
+            _dispTimer1.Tick += dispTimer_Tick1;
+            _dispTimer1.Start();
+        }
+
+        private void StopWork(object sender, RoutedEventArgs e)
+        {
+            _dispTimer1.Stop();
+        }
+
+        private void RestartButtonWork(object sender, RoutedEventArgs e)
+        {
+            _dispTimerCounterWork = MaxTimerCounterWork;
+            Timer1.Text = (_dispTimerCounterWork--).ToString();
+            _dispTimer1.Stop();
+        }
+
+        void dispTimer_Tick2(object sender, EventArgs e)
+        {
+            //сначала выводится значение, потом инкрементируется (не для всех очевидно)
+            Timer2.Text = (_dispTimerCounterRelax--).ToString();
+            if (_dispTimerCounterRelax == MinTimerCounter)
+            {
+                _dispTimer2.Stop();
+                SystemSounds.Beep.Play();
+            }
+
+        }
+
+        private void StartRlx(object sender, RoutedEventArgs e)
+        {
+            MaxTimerCounterRelax = Convert.ToInt32(Relax.Text);
+            _dispTimerCounterRelax = MaxTimerCounterRelax;
+            _dispTimer2 = new DispatcherTimer();
+            _dispTimer2.Interval = TimeSpan.FromSeconds(1);
+            _dispTimer2.Tick += dispTimer_Tick2;
+            _dispTimer2.Start();
+        }
+
+        private void StopRlx(object sender, RoutedEventArgs e)
+        {
+            _dispTimer2.Stop();
         }
     }
 }
